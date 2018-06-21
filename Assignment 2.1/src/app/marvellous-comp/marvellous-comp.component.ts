f2198dcef30341c876edcce38b9aa5aeaa222e05
StableWeaver <38763239+StableWeaver@users.noreply.github.com>
2018-06-21 21:38:37 +0530

    Assignment 2
    includes assignment 2.1

---
+++ Assignment 2.1/src/app/marvellous-comp/marvellous-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marvellous-comp',
+  templateUrl: './marvellous-comp.component.html',
+  styleUrls: ['./marvellous-comp.component.css']
+})
+export class MarvellousCompComponent implements OnInit {
+Name= 'Marvellous-Comp9';                                             //Question 5 assignment 2.
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
